5fcfc5b1a67a9bb0064ad9ca117544ac31795c59
karine2711 <karine.gevorgyan.2001@mail.ru>
2019-12-11 22:29:26 +0400

    Added a constructor to Human.java

--- src/Homework_13_12_2019/Human.java
+++ src/Homework_13_12_2019/Human.java
@@ -3,7 +3,15 @@ package Homework_13_12_2019;
 public class Human {
     public String firstname;
     public String lastname;
+    public char gender;
     public String age;
     public float rating;
-    public char gender;
+
+    public Human(String firstname, String lastname, char gender, String age, float rating) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.gender = gender;
+        this.age = age;
+        this.rating = rating;
+    }
 }
