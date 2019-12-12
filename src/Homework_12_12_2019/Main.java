f1e083b73fb285ff957a16d7467443a5c0e09eaa
karine2711 <karine.gevorgyan.2001@mail.ru>
2019-12-12 19:54:00 +0400

    Added solution for '1. Հայտարարել զանգված և տպել այն։' to Main.java

--- src/Homework_12_12_2019/Main.java
+++ src/Homework_12_12_2019/Main.java
@@ -3,5 +3,12 @@ package Homework_12_12_2019;
 public class Main {
     public static void main(String[] args) {
 
+        //1. Հայտարարել զանգված և տպել այն։
+        System.out.println("1. Հայտարարել զանգված և տպել այն։");
+        String[] names={"Karine", "Marine", "Serine", "Narine", "Poxos", "Petros", "Martiros"};
+        for(String name:names)
+            System.out.println(name);
+
+
     }
 }
