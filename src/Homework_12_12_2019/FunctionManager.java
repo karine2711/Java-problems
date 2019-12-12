aa420ea38b13b8d95da20b13c0cfc91068a2c3a0
karine2711 <karine.gevorgyan.2001@mail.ru>
2019-12-12 22:01:57 +0400

    Added  class FunctionManager.java and implemented method for '1.Parameter stanal 2 int ev veradarcnel gmuar@'

---
+++ src/Homework_12_12_2019/FunctionManager.java
@@ -0,0 +1,9 @@
+package Homework_12_12_2019;
+
+public class FunctionManager {
+//    1. parameter stanal 2 int veradarcnel dranc gumary
+    public int sumOfNumbers(int x, int y){
+        return x+y;
+    }
+
+}
